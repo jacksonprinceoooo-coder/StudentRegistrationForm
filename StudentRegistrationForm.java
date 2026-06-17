26b98a940fb1f91474834f8a9a2daefcf278f934
jacksonprinceoooo-coder <jacksonprinceoooo@gmail.com>
2026-06-17 19:31:15 +0530

    Add Student Registration Form GUI

---
+++ StudentRegistrationForm.java
@@ -0,0 +1,170 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class StudentRegistrationForm extends JFrame implements ActionListener {
+
+    JLabel lblTitle, lblName, lblRollNo, lblGender, lblDept,
+            lblYear, lblEmail, lblPhone, lblAddress;
+
+    JTextField txtName, txtRollNo, txtEmail, txtPhone;
+    JTextArea txtAddress;
+
+    JRadioButton rbMale, rbFemale;
+    ButtonGroup genderGroup;
+
+    JComboBox<String> cmbDept, cmbYear;
+
+    JButton btnSubmit, btnClear;
+
+    StudentRegistrationForm() {
+
+        setTitle("Student Registration Form");
+        setSize(600, 600);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        lblTitle = new JLabel("STUDENT REGISTRATION FORM");
+        lblTitle.setBounds(180, 20, 300, 30);
+        add(lblTitle);
+
+        lblName = new JLabel("Student Name:");
+        lblName.setBounds(50, 70, 120, 25);
+        add(lblName);
+
+        txtName = new JTextField();
+        txtName.setBounds(200, 70, 250, 25);
+        add(txtName);
+
+        lblRollNo = new JLabel("Roll Number:");
+        lblRollNo.setBounds(50, 110, 120, 25);
+        add(lblRollNo);
+
+        txtRollNo = new JTextField();
+        txtRollNo.setBounds(200, 110, 250, 25);
+        add(txtRollNo);
+
+        lblGender = new JLabel("Gender:");
+        lblGender.setBounds(50, 150, 120, 25);
+        add(lblGender);
+
+        rbMale = new JRadioButton("Male");
+        rbMale.setBounds(200, 150, 80, 25);
+
+        rbFemale = new JRadioButton("Female");
+        rbFemale.setBounds(300, 150, 100, 25);
+
+        genderGroup = new ButtonGroup();
+        genderGroup.add(rbMale);
+        genderGroup.add(rbFemale);
+
+        add(rbMale);
+        add(rbFemale);
+
+        lblDept = new JLabel("Department:");
+        lblDept.setBounds(50, 190, 120, 25);
+        add(lblDept);
+
+        String depts[] = {"CSE", "IT", "ECE", "EEE", "MECH", "CIVIL"};
+        cmbDept = new JComboBox<>(depts);
+        cmbDept.setBounds(200, 190, 250, 25);
+        add(cmbDept);
+
+        lblYear = new JLabel("Year:");
+        lblYear.setBounds(50, 230, 120, 25);
+        add(lblYear);
+
+        String years[] = {"I Year", "II Year", "III Year", "IV Year"};
+        cmbYear = new JComboBox<>(years);
+        cmbYear.setBounds(200, 230, 250, 25);
+        add(cmbYear);
+
+        lblEmail = new JLabel("Email:");
+        lblEmail.setBounds(50, 270, 120, 25);
+        add(lblEmail);
+
+        txtEmail = new JTextField();
+        txtEmail.setBounds(200, 270, 250, 25);
+        add(txtEmail);
+
+        lblPhone = new JLabel("Phone Number:");
+        lblPhone.setBounds(50, 310, 120, 25);
+        add(lblPhone);
+
+        txtPhone = new JTextField();
+        txtPhone.setBounds(200, 310, 250, 25);
+        add(txtPhone);
+
+        lblAddress = new JLabel("Address:");
+        lblAddress.setBounds(50, 350, 120, 25);
+        add(lblAddress);
+
+        txtAddress = new JTextArea();
+        JScrollPane sp = new JScrollPane(txtAddress);
+        sp.setBounds(200, 350, 250, 80);
+        add(sp);
+
+        btnSubmit = new JButton("Submit");
+        btnSubmit.setBounds(150, 470, 100, 30);
+        btnSubmit.addActionListener(this);
+        add(btnSubmit);
+
+        btnClear = new JButton("Clear");
+        btnClear.setBounds(300, 470, 100, 30);
+        btnClear.addActionListener(this);
+        add(btnClear);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == btnSubmit) {
+
+            String name = txtName.getText();
+            String rollNo = txtRollNo.getText();
+
+            String gender = "";
+            if (rbMale.isSelected())
+                gender = "Male";
+            else if (rbFemale.isSelected())
+                gender = "Female";
+
+            String dept = cmbDept.getSelectedItem().toString();
+            String year = cmbYear.getSelectedItem().toString();
+            String email = txtEmail.getText();
+            String phone = txtPhone.getText();
+            String address = txtAddress.getText();
+
+            String message =
+                    "Registration Successful!\n\n" +
+                    "Name: " + name + "\n" +
+                    "Roll No: " + rollNo + "\n" +
+                    "Gender: " + gender + "\n" +
+                    "Department: " + dept + "\n" +
+                    "Year: " + year + "\n" +
+                    "Email: " + email + "\n" +
+                    "Phone: " + phone + "\n" +
+                    "Address: " + address;
+
+            JOptionPane.showMessageDialog(this, message);
+        }
+
+        if (e.getSource() == btnClear) {
+
+            txtName.setText("");
+            txtRollNo.setText("");
+            txtEmail.setText("");
+            txtPhone.setText("");
+            txtAddress.setText("");
+
+            genderGroup.clearSelection();
+
+            cmbDept.setSelectedIndex(0);
+            cmbYear.setSelectedIndex(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        new StudentRegistrationForm();
+    }
+              }
